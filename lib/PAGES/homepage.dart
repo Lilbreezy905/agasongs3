0004d9676e9162bf05b13a1e66439e3b588305bc
BIZIMANA Beni Brice <benibrice2@gmail.com>
2024-07-17 15:15:03 +0200

    updating components

--- lib/PAGES/homepage.dart
+++ lib/PAGES/homepage.dart
@@ -2,103 +2,169 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:uhuru_music_stable/Widgets/musiclist.dart';
-import 'package:uhuru_music_stable/Widgets/playlist_widget.dart';
+
 import 'package:uhuru_music_stable/PAGES/music_list.dart';
 import 'package:uhuru_music_stable/controller/connectivity.dart';
+import 'package:uhuru_music_stable/utilities/app_theme.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late final TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  // _changeTab() {
+  //   if (_tabController.index == 0) {
+  //     if (mounted) {
+  //       setState(() {
+  //         Variables.isMusicTab = true;
+  //         Variables.isTredingTab = false;
+  //         Variables.isNewTab = false;
+  //         Variables.baseQuery = 'music';
+  //         Variables.query = 'music';
+  //         if (kDebugMode) {
+  //           print('====>>> variables query contains  ${Variables.baseQuery}');
+  //         }
+  //       });
+  //     }
+  //   } else if (_tabController.index == 1) {
+  //     if (mounted) {
+  //       setState(() {
+  //         Variables.isNewTab = true;
+  //         Variables.isMusicTab = false;
+  //         Variables.isTredingTab = false;
+  //         Variables.baseQuery = ' new music 2024';
+  //         Variables.query = 'new music ';
+  //         if (kDebugMode) {
+  //           print('====>>> variables query contains  ${Variables.baseQuery}');
+  //         }
+  //       });
+  //     }
+  //   } else {
+  //     if (mounted) {
+  //       setState(() {
+  //         Variables.isTredingTab = true;
+  //         Variables.isMusicTab = false;
+  //         Variables.isNewTab = false;
+  //         Variables.baseQuery = 'treding music';
+  //         Variables.query = 'treding music';
+  //         if (kDebugMode) {
+  //           print('====>>> variables query contains  ${Variables.baseQuery}');
+  //         }
+  //       });
+  //     }
+  //   }
+  // }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.center,
-                end: Alignment.bottomCenter,
-                colors: [
-              Color(0xFF303151).withOpacity(0.6),
-              Color(0xFF303151).withOpacity(0.9)
-            ])),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: GetBuilder<ConnectivityCheck>(
-              init: ConnectivityCheck(),
-              builder: (controller) {
-                return SafeArea(
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 20, top: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 5),
-                          child: Text(
-                            'uhuru music',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                letterSpacing: 1,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        TabBar(
-                          isScrollable: true,
-                          labelStyle: TextStyle(fontSize: 20),
-                          indicator: BoxDecoration(
-                              border: Border(
-                                  bottom: BorderSide(
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.center,
+              end: Alignment.bottomCenter,
+              colors: [
+            Color(0xFF303151).withOpacity(0.6),
+            Color(0xFF303151).withOpacity(0.9)
+          ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: GetBuilder<ConnectivityCheck>(
+          init: ConnectivityCheck(),
+          builder: (controller) {
+            return SafeArea(
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, top: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 5),
+                      child: Text(
+                        'Uhuru music',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            letterSpacing: 1,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    TabBar(
+                      controller: _tabController,
+                      isScrollable: true,
+                      labelStyle: TextStyle(
+                        fontSize: 20,
+                        color: AppTheme.white,
+                      ),
+                      indicator: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
                             width: 3,
                             color: Colors.pink,
-                          ))),
-                          tabs: [
-                            Tab(
-                              text: 'music',
-                            ),
-                            Tab(
-                              text: 'new',
-                            ),
-                            Tab(
-                              text: 'treding',
-                            )
-                          ],
-                        ),
-                        Flexible(
-                          flex: 1,
-                          child: TabBarView(
-                            children: [
-                              controller.hasConnection
-                                  ? MusicListDetail(
-                                      query: 'music usa ',
-                                    )
-                                  : Center(
-                                      child: Text('No internet connection'),
-                                    ),
-                              controller.hasConnection
-                                  ? MusicListDetail(
-                                      query: 'new music 2024 ',
-                                    )
-                                  : Center(
-                                      child: Text('No internet connection'),
-                                    ),
-                              controller.hasConnection
-                                  ? MusicListDetail(
-                                      query: 'treding music ',
-                                    )
-                                  : Center(
-                                      child: Text('No internet connection'),
-                                    ),
-                            ],
                           ),
+                        ),
+                      ),
+                      tabs: const [
+                        Tab(
+                          text: 'music',
+                        ),
+                        Tab(
+                          text: 'new',
+                        ),
+                        Tab(
+                          text: 'treding',
                         )
                       ],
                     ),
-                  ),
-                );
-              }),
+                    Flexible(
+                      flex: 1,
+                      child: TabBarView(
+                        controller: _tabController,
+                        children: [
+                          controller.hasConnection
+                              ? FirstMusicDetail(
+                                  query: 'music usa ',
+                                )
+                              : Center(
+                                  child: Text('No internet connection'),
+                                ),
+                          controller.hasConnection
+                              ? SecondMusicListDetail(
+                                  query: 'new music 2024 ',
+                                )
+                              : Center(
+                                  child: Text('No internet connection'),
+                                ),
+                          controller.hasConnection
+                              ? ThirdMusicDetail(
+                                  query: 'treding music ',
+                                )
+                              : Center(
+                                  child: Text('No internet connection'),
+                                ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
--- lib/env/variables.dart
+++ lib/env/variables.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Variables {
   ///variables a utiliser pour recuper les valeurs dans l'api
 
@@ -5,9 +7,54 @@ class Variables {
   static String? pageToken;
   static String? searchPageToken;
   static String? videoId;
+  static String? baseQuery;
+  static String? query;
+  static List<Map<String, String>> queryList = [
+    {
+      'Afro': 'afro $baseQuery',
+      'Jazz': 'jazz $baseQuery',
+      'Pop': 'pop $baseQuery',
+      'Hip-hop': 'hip-hop $baseQuery',
+      'Rap': 'rap $baseQuery',
+      'Electonic': 'electronic $baseQuery',
+      'R&B': 'R&B $baseQuery',
+      'Latino': 'latino $baseQuery',
+      'Gospel': 'gospel $baseQuery',
+      'Blues': 'blues $baseQuery',
+      'Classic': 'classic $baseQuery',
+      'Reggae': 'reggae $baseQuery',
+      'Dancehall': 'dancehall $baseQuery',
+      'Electonic Techno': 'electonic techno $baseQuery',
+    }
+  ];
+  static CircularProgressIndicator indicator({required Color color}) {
+    return CircularProgressIndicator(
+      color: color,
+    );
+  }
 
   static String? songName;
   static String? singerName;
   static bool loadData = false;
   static String? url;
+  static bool isMusicTab = false;
+  static bool isNewTab = false;
+  static bool isTredingTab = false;
+  static List<String> musicCategories = [
+    'All Categories :',
+    'Afro',
+    'Jazz',
+    'Pop',
+    'Hip-hop',
+    'Rap',
+    'Electonic',
+    'R&B',
+    'Latino',
+    'Gospel',
+    'Blues',
+    'Classic',
+    'Reggae',
+    'Dancehall',
+    'Electonic Techno',
+  ];
 }
